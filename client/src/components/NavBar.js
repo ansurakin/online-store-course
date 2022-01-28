9839077aac0e26c32c8a4c69498c0b33ce1032b7
ansurakin <surakin@vtb.ru>
2022-01-28 12:19:23 +0300

    Админ панель

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
-import {SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 
 const NavBar = observer(() => {
   const {user} = useContext(Context)
+  const navigate = useNavigate()
   return (
       <Navbar bg="dark" variant="dark">
         <Container>
@@ -14,9 +15,17 @@ const NavBar = observer(() => {
           {
             user.isAuth ?
                 <Nav className="ms-auto" style={{color: "white"}}>
-                  <Button variant={"outline-light"}>Админ панель</Button>
                   <Button variant={"outline-light"}
-                          className="ms-2">Выйти</Button>
+                          onClick={() => navigate(ADMIN_ROUTE)}
+                  >
+                    Админ панель
+                  </Button>
+                  <Button variant={"outline-light"}
+                          className="ms-2"
+                          onClick={() => navigate(LOGIN_ROUTE)}
+                  >
+                    Выйти
+                  </Button>
                 </Nav>
                 :
                 <Nav className="ms-auto" style={{color: "white"}}>
